8bd557f80d65b0ff86c101ab49db4eac73f970f8
lcsan <long69954547@hotmail.com>
2018-12-04 18:57:55 +0800

    Update ExtractBy.java

--- webmagic-extension/src/main/java/us/codecraft/webmagic/model/annotation/ExtractBy.java
+++ webmagic-extension/src/main/java/us/codecraft/webmagic/model/annotation/ExtractBy.java
@@ -17,53 +17,60 @@ import us.codecraft.webmagic.selector.Type;
 @Target({ ElementType.FIELD, ElementType.TYPE })
 public @interface ExtractBy {
 
-	/**
-	 * Extractor expression, support XPath, CSS Selector and regex.
-	 *
-	 * @return extractor expression
-	 */
-	String value();
+    /**
+     * Extractor expression, support XPath, CSS Selector and regex.
+     *
+     * @return extractor expression
+     */
+    String value();
 
-	/**
-	 * types of extractor expressions
-	 */
-	// public static enum Type {XPath, Regex, Css, JsonPath}
+    /**
+     * more argument
+     * 
+     * @return
+     */
+    String[] args() default {};
 
-	/**
-	 * Extractor type, support XPath, CSS Selector and regex.
-	 *
-	 * @return extractor type
-	 */
-	Type type() default Type.mixe;
+    /**
+     * types of extractor expressions
+     */
+    // public static enum Type {XPath, Regex, Css, JsonPath}
 
-	/**
-	 * Define whether the field can be null.<br>
-	 * If set to 'true' and the extractor get no result, the entire class will
-	 * be discarded. <br>
-	 *
-	 * @return whether the field can be null
-	 */
-	boolean notNull() default false;
+    /**
+     * Extractor type, support XPath, CSS Selector and regex.
+     *
+     * @return extractor type
+     */
+    Type type() default Type.mixe;
 
-	/**
-	 * The source for extracting. <br>
-	 * It works only if you already added 'ExtractBy' to Class. <br>
-	 *
-	 * @return the source for extracting
-	 */
-	Source source() default Source.SelectedHtml;
+    /**
+     * Define whether the field can be null.<br>
+     * If set to 'true' and the extractor get no result, the entire class will
+     * be discarded. <br>
+     *
+     * @return whether the field can be null
+     */
+    boolean notNull() default false;
 
-	/**
-	 * Define whether the extractor return more than one result. When set to
-	 * 'true', the extractor return a list of string (so you should define the
-	 * field as List). <br>
-	 *
-	 * Deprecated since 0.4.2. This option is determined automatically by the
-	 * class of field.
-	 * 
-	 * @deprecated since 0.4.2
-	 * @return whether the extractor return more than one result
-	 */
-	boolean multi() default false;
+    /**
+     * The source for extracting. <br>
+     * It works only if you already added 'ExtractBy' to Class. <br>
+     *
+     * @return the source for extracting
+     */
+    Source source() default Source.SelectedHtml;
+
+    /**
+     * Define whether the extractor return more than one result. When set to
+     * 'true', the extractor return a list of string (so you should define the
+     * field as List). <br>
+     *
+     * Deprecated since 0.4.2. This option is determined automatically by the
+     * class of field.
+     * 
+     * @deprecated since 0.4.2
+     * @return whether the extractor return more than one result
+     */
+    boolean multi() default false;
 
 }
