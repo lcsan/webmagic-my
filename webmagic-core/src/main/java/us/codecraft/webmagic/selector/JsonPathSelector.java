cfc7173a48779851c4768b2223885012367e9f60
lcsan <long69954547@hotmail.com>
2018-12-04 10:47:56 +0800

    Update JsonPathSelector.java

--- webmagic-core/src/main/java/us/codecraft/webmagic/selector/JsonPathSelector.java
+++ webmagic-core/src/main/java/us/codecraft/webmagic/selector/JsonPathSelector.java
@@ -16,75 +16,75 @@ import com.jayway.jsonpath.JsonPath;
  */
 public class JsonPathSelector implements Selector {
 
-	private String jsonPathStr;
+    private String jsonPathStr;
 
-	private JsonPath jsonPath;
+    private JsonPath jsonPath;
 
-	public JsonPathSelector(String jsonPathStr) {
-		this.jsonPathStr = jsonPathStr;
-		this.jsonPath = JsonPath.compile(this.jsonPathStr);
-	}
+    public JsonPathSelector(String jsonPathStr) {
+        this.jsonPathStr = jsonPathStr;
+        this.jsonPath = JsonPath.compile(this.jsonPathStr);
+    }
 
-	private String realJson(String text) {
-		if (!text.startsWith("{") && !text.startsWith("[")) {
-			int a = text.indexOf("{");
-			int b = text.indexOf("[");
-			int start = Math.min(a, b);
-			// 解决-1
-			start = start == -1 ? (a > -1 ? a : (b > -1 ? b : 0)) : start;
-			int end = Math.max(text.lastIndexOf("}"), text.lastIndexOf("]")) + 1;
-			text = text.substring(start, end);
-		}
-		return text;
-	}
+    private String realJson(String text) {
+        if (!text.startsWith("{") && !text.startsWith("[")) {
+            int a = text.indexOf("{");
+            int b = text.indexOf("[");
+            int start = Math.min(a, b);
+            // 解决-1
+            start = start == -1 ? (a > -1 ? a : (b > -1 ? b : 0)) : start;
+            int end = Math.max(text.lastIndexOf("}"), text.lastIndexOf("]")) + 1;
+            text = text.substring(start, end);
+        }
+        return text;
+    }
 
-	@Override
-	public String select(String text) {
-		text = realJson(text);
-		Object object = jsonPath.read(text);
-		if (object == null) {
-			return null;
-		}
-		if (object instanceof List) {
-			List list = (List) object;
-			if (list != null && list.size() > 0) {
-				return toString(list.iterator().next());
-			}
-		}
-		return object.toString();
-	}
+    @Override
+    public String select(String text) {
+        text = realJson(text);
+        Object object = jsonPath.read(text);
+        if (object == null) {
+            return null;
+        }
+        if (object instanceof List) {
+            List<?> list = (List<?>) object;
+            if (list != null && list.size() > 0) {
+                return toString(list.iterator().next());
+            }
+        }
+        return object.toString();
+    }
 
-	private String toString(Object object) {
-		if (object instanceof Map) {
-			return JSON.toJSONString(object);
-		} else {
-			return String.valueOf(object);
-		}
-	}
+    private String toString(Object object) {
+        if (object instanceof Map) {
+            return JSON.toJSONString(object);
+        } else {
+            return String.valueOf(object);
+        }
+    }
 
-	@Override
-	public List<String> selectList(String text) {
-		List<String> list = new ArrayList<String>();
-		if (null == text) {
-			return list;
-		}
-		text = realJson(text);
-		if ("".equals(text)) {
-			return list;
-		}
+    @Override
+    public List<String> selectList(String text) {
+        List<String> list = new ArrayList<String>();
+        if (null == text) {
+            return list;
+        }
+        text = realJson(text);
+        if ("".equals(text)) {
+            return list;
+        }
 
-		Object object = jsonPath.read(text);
-		if (object == null) {
-			return list;
-		}
-		if (object instanceof List) {
-			List<Object> items = (List<Object>) object;
-			for (Object item : items) {
-				list.add(toString(item));
-			}
-		} else {
-			list.add(toString(object));
-		}
-		return list;
-	}
+        Object object = jsonPath.read(text);
+        if (object == null) {
+            return list;
+        }
+        if (object instanceof List) {
+            List<?> items = (List<?>) object;
+            for (Object item : items) {
+                list.add(toString(item));
+            }
+        } else {
+            list.add(toString(object));
+        }
+        return list;
+    }
 }
