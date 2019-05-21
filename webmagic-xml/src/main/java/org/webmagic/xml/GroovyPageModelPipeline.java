500e7b52ad09b0a59210e512576392d6f5e45950
lcsan <long69954547@hotmail.com>
2019-05-21 14:09:07 +0800

    Update GroovyPageModelPipeline.java

--- webmagic-xml/src/main/java/org/webmagic/xml/GroovyPageModelPipeline.java
+++ webmagic-xml/src/main/java/org/webmagic/xml/GroovyPageModelPipeline.java
@@ -14,39 +14,39 @@ import us.codecraft.webmagic.pipeline.PageModelPipeline;
 
 public class GroovyPageModelPipeline implements PageModelPipeline<Object> {
 
-	private Invocable inv;
+    private Invocable inv;
 
-	public GroovyPageModelPipeline() {
-		super();
-	}
+    public GroovyPageModelPipeline() {
+        super();
+    }
 
-	public GroovyPageModelPipeline(String source) {
-		super();
-		if (StringUtils.isNotBlank(source)) {
-			ScriptEngineManager factory = new ScriptEngineManager();
-			ScriptEngine engine = factory.getEngineByName("groovy");
-			try {
-				engine.eval("import org.webmagic.*;\r\ndef pipeline(obj,task){" + source + "}");
-			} catch (ScriptException e) {
-				e.printStackTrace();
-			}
-			inv = (Invocable) engine;
-		}
-	}
+    public GroovyPageModelPipeline(String source) {
+        super();
+        if (StringUtils.isNotBlank(source)) {
+            ScriptEngineManager factory = new ScriptEngineManager();
+            ScriptEngine engine = factory.getEngineByName("groovy");
+            try {
+                engine.eval(source);
+            } catch (ScriptException e) {
+                e.printStackTrace();
+            }
+            inv = (Invocable) engine;
+        }
+    }
 
-	@Override
-	public void process(Object t, Task task) {
-		if (null != inv) {
-			try {
-				inv.invokeFunction("pipeline", t, task);
-			} catch (NoSuchMethodException e) {
-				e.printStackTrace();
-			} catch (ScriptException e) {
-				e.printStackTrace();
-			}
-		} else {
-			System.out.println(JSON.toJSONString(t));
-		}
-	}
+    @Override
+    public void process(Object t, Task task) {
+        if (null != inv) {
+            try {
+                inv.invokeFunction("pipeline", t, task);
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (ScriptException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println(JSON.toJSONString(t));
+        }
+    }
 
 }
