b18cd922ce2973fed07a892919f876f0da955cfa
lcsan <long69954547@hotmail.com>
2019-05-21 14:12:50 +0800

    Add files via upload

---
+++ webmagic-xml/src/main/java/org/webmagic/xml/bean/GroovyPipeline.java
@@ -0,0 +1,34 @@
+package org.webmagic.xml.bean;
+
+import javax.xml.bind.annotation.XmlValue;
+
+import org.webmagic.xml.GroovyPageModelPipeline;
+
+public class GroovyPipeline {
+
+    private String code;
+
+    private GroovyPageModelPipeline pipeline;
+
+    public String getCode() {
+        return code;
+    }
+
+    @XmlValue
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public GroovyPageModelPipeline getPipeline() {
+        if (null == pipeline) {
+            pipeline = new GroovyPageModelPipeline(code);
+        }
+        return pipeline;
+    }
+
+    @Override
+    public String toString() {
+        return "Pipeline [code=" + code + "]";
+    }
+
+}
